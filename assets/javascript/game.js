293a02b34eb9b74ef9bb1b295decca577baee7c9
Daniel.Elston <danielelston94@gmail.com>
2018-07-19 21:30:07 -0400

    java update for other machines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,27 @@
-var character = {
+const character = {
     charName:"filler", 
     HP:1, 
-    attk:0, 
+    attk:0,
+    heavyAttk:0,
+    specialAttk:0, 
     counterAttk:0
 };
+
+// Fight logic-----------------
+
+
+  var cloud = Object.assign({charName: "Cloud", hp: 4, attk: 5, heavyAttk:0, specialAttk:0, counterAttk:0}, character);
+  var sephiroth= Object.assign({charName: "Sephiroth", hp: 4, attk: 5, heavyAttk:0, specialAttk:0, counterAttk:0}, character);
+  var noctis = Object.assign({charName: "Noctis", hp: 4, attk: 5, heavyAttk:0, specialAttk:0, counterAttk:0}, character);
+  var lightning = Object.assign({charName: "Lightning", hp: 4, attk: 5, heavyAttk:0, specialAttk:0, counterAttk:0}, character);
+
+
+  
+  console.log(cloud.hp, cloud.attk);
+  console.log(sephiroth.hp, sephiroth.attk);
+  console.log(noctis.hp, noctis.attk);
+  console.log(lightning.hp, lightning.attk);
+
+
+  // expected output: 3 5
 
